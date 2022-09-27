0cdd9595f49c44ba62adf54bdd6677f6154a9eb4
greenolls <a.kuleshov@hypeauditor.com>
2022-09-27 14:48:48 +0300

    finished

---
+++ src/test/java/RestAssuredReview/PraktikumTest.java
@@ -0,0 +1,56 @@
+package RestAssuredReview;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class PraktikumTest {
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
+    }
+
+    @Test
+    public void registrationAndAuth() {
+        // Составили email
+        Random random = new Random();
+        String email = "something" + random.nextInt(10000000) + "@yandex.ru";
+        // составь json, используя переменную email. Не забудь про экранизацию кавычек с помощью '/'
+        String json = "{\"email\": \"" + email + "\",\"password\": \"password\"}";
+
+// POST запрос на регистрацию signup
+        given()
+                .body(json)
+                .contentType("application/json")
+                .post("/api/signup")
+                .then()
+        // проверь статус ответа
+                .statusCode(201);
+
+        // POST запрос на авторизацию signin с теми же параметрами
+        Response response = given()
+                .body(json)
+                .contentType("application/json")
+                .post("/api/signin");
+        response.then().assertThat()
+        // проверь, что пришедший в ответ токен не пустой
+                .body("token", notNullValue())
+                 .and()
+        // проверь статус ответа
+                .statusCode(200);
+        // Попытка зарегистрироваться с теми же параметрами ещё раз
+        given()
+                .body(json)
+                .contentType("application/json")
+                .post("/api/signup")
+                .then()
+        // проверь, что статус ответа изменился
+                .statusCode(409);
+    }
+}
